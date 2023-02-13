b7bde3bdb15309d503fbef0f151594ff28e9889b
m-hayashi13 <m-hayashi@advanced-media.co.jp>
2023-02-13 14:56:05 +0900

    Initial Commit

---
+++ ChatGPTwithSpeech/ChatGPTwithSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTwithSpeechApp.swift
+//  ChatGPTwithSpeech
+//
+//  Created by 林 政樹 on 2023/02/13.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTwithSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
